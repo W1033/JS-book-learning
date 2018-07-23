2bbc991cecdd090739b2bf607bc12728bc2e19c3
Payne <2416451355@qq.com>
2018-07-23 10:18:06 +0800

    Update Promie-source-code.js

--- 《深入理解ES6》/11th-chapter--Promise/Promie-source-code.js
+++ 《深入理解ES6》/11th-chapter--Promise/Promie-source-code.js
@@ -1,5 +1,5 @@
-// js 异步之 Promise 源码分析 ()
-
+// [js 异步之 Promise 源码分析] (https://juejin.im/entry/599968f6518825244630f809)
+// https://download.csdn.net/download/tydqcjj/10036125
 
 // 最簡單的異步執行
 let basicAsync = (callback) => {
@@ -8,3 +8,19 @@ let basicAsync = (callback) => {
 basicAsync(function(data) { console.log(data) });   // 1s 后輸出1
 
 
+
+// 添加代码执行错误时给出提示
+let maybeAfterOneSecond = function(callback, errorback) {
+    setTimeout(function() {
+        // 进行判断情况，是执行成功的回调，还是执行错误的回调
+        if (Math.random() < .5) {
+            callback("输出1");
+        } else {
+           errorback(new Error("Can't provide one."))
+        }
+    }, 1000);
+};
+maybeAfterOneSecond(
+    function(data) { console.log(data); },
+    function(err) { console.log(err); }
+);
