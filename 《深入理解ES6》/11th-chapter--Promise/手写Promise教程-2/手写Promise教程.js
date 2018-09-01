b2472d1a895a6bf69b9f67194f7ab7af6abce280
2416451355@qq.com <314314@wang>
2018-09-01 15:28:50 +0800

    edit:180901

--- 《深入理解ES6》/11th-chapter--Promise/手写Promise教程-2/手写Promise教程.js
+++ 《深入理解ES6》/11th-chapter--Promise/手写Promise教程-2/手写Promise教程.js
@@ -1,5 +1,5 @@
 // Promises/A+ 规范: https://promisesaplus.com/
-
+// [BAT前端经典面试问题：史上最最最详细的手写Promise教程](https://juejin.im/post/5b2f02cd5188252b937548ab)
 class Promise {
     // executor /ɪg'zekjʊtə/ n.执行者
     constructor(executor) {
