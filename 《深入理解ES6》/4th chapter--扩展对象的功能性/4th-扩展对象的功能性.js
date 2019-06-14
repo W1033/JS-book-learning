30097c2464ab9dd64bd4a4109213db3d738e02fa
2416451355@qq.com <314314@wang>
2019-06-14 14:46:50 +0800

    edit:20190614

--- 《深入理解ES6》/4th chapter--扩展对象的功能性/4th-扩展对象的功能性.js
+++ 《深入理解ES6》/4th chapter--扩展对象的功能性/4th-扩展对象的功能性.js
@@ -44,10 +44,10 @@ const store = new Vuex.Store({
 });
 
 
-// 20180501-P76 - 新增方法: Object.is(): 弥补全等运算符的不准确运算。 比如之前 +0等于-0, NaN不等于NaN, 使用后:
+// 20180501-P76 - 新增方法: Object.is(): 弥补全等运算符的不准确运算。 比如之前
+// +0等于-0, NaN不等于NaN, 使用后:
 console.log(+0 === -0);            // true
 console.log(NaN === NaN);          // false
-
 
 console.log(Object.is(+0, -0));   // false
 console.log(Object.is(NaN, NaN));  // true
@@ -55,23 +55,30 @@ console.log(Object.is(NaN, NaN));  // true
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// 20180501-P76 - 新增方法: Object.assign():
-// 20180502: 混合(Mixin)是 js 中实现对象组合的一种模式。在一个 mixin 方法中，一个对象
-// 接受来自另一个对象的属性和方法，
-// 许多 js 库中都有类似的 mixin 方法:
-// receiver /rɪ'siːvə/ n.接收器  supplier /səˈplaɪə/ n.提供者
+/* - assign [ə'saɪn] ~~~v.分配，指定, 派，调派
+ * - receiver /rɪ'siːvə/ ~~~n.接收器
+ * - supplier /səˈplaɪə/ ~~~n.提供者*/
+
+/* - 20180501: P76-新增方法: Object.assign():
+ * - 20180502: 混合(Mixin)是 js 中实现对象组合的一种模式。在一个 mixin 方法中，一个对象
+ *   接受来自另一个对象的属性和方法。许多 js 库中都有类似的 mixin 方法: */
+/* - (1)、
+ *   + Object.keys() 方法取得对象上所有可枚举的实例属性。这个方法接受一个对象作为参数，
+ *      返回一个包含所有可枚举属性的字符串数组。
+ *   + 示例见: 《js高级程序设计》\js高程学习笔记\js高程--Object.keys().js
+ *
+ * - (2)、
+ *   + mixin() 函数遍历 supplier 的自有属性赋并复制到 receiver 中(此处的复制只是浅复制，
+ *      当属性值为对象时只复制对象的引用)。这样一来，receiver 不通过继承就可以获得新属性，
+ *      请参考这段代码:
+ */
 function mixin(receiver, supplier) {
-    // github-clone\js-sundry-goods\js--A语法--MDN文档\js高程---Object.keys().js
-    // Object.keys() 方法取得对象上所有可枚举的实例属性。这个方法接受一个对象作为参数，
-    // 返回一个包含所有可枚举属性的字符串数组。
+    // (1)、
     Object.keys(supplier).forEach(function (key) {
         receiver[key] = supplier[key];
     })
 }
 
-// mixin() 函数遍历 supplier 的自有属性赋并复制到 receiver 中(此处的复制只是浅复制，
-// 当属性值为对象时只复制对象的引用)。
-// 这样一来，receiver 不通过继承就可以获得新属性，请参考这段代码:
 function EventTarget() { }
 EventTarget.prototype = {
     constructor: EventTarget,
@@ -82,14 +89,16 @@ EventTarget.prototype = {
     }
 };
 const myObj = {};
+// (2)、
 mixin(myObj, EventTarget.prototype);
 
 // myObj 接受 EventTarget.prototype 对象的所有行为从而使 myObj 可以通过 emit() 方法
 // 发布事件或通过 on() 方法订阅事件。
 myObj.emit("somethingChanged");     // somethingChanged
 
-// 上面的 mixin() 调用可以直接替换为 Object.assign()
-// 201809015-add: 即使这样 Object.assign() 仍然是潜拷贝，深拷贝见: JS集锦.js
+/* - 上面的 mixin() 调用可以直接替换为 -- ES6 的 Object.assign()
+ * - 201809015-add: 即使这样 Object.assign() 仍然是潜拷贝。 深拷贝见:
+ * - js-sundry-goods\20190405-浅拷贝+深拷贝\20190108 jq拷贝继承(深拷贝).html */
 Object.assign(myObj, EventTarget.prototype);
 myObj.emit("somethingChange again");
 
