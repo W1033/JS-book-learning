7988177bccf409ad8bc7b817cb433cb412617c97
2416451355@qq.com <314314@wang>
2019-07-04 13:15:50 +0800

    edit:20190704

--- 《You-Dont-Know-JS》/《You-Dont-Know-JS》-中卷/Async-Performance/3.1.1_未来值.js
+++ 《You-Dont-Know-JS》/《You-Dont-Know-JS》-中卷/Async-Performance/3.1.1_未来值.js
@@ -22,51 +22,18 @@ function add(xPromise, yPromise) {
 }
 
 
-// function fetchX() {
-//     return new Promise(function (resolve, reject) {
-//         resolve(200)
-//     })
-// }
-// function fetchY() {
-//     return new Promise(function (resolve, reject) {
-//         resolve(4)
-//     })
-// }
-//
-// // fetchX() 和 fetchY() 返回相应值的 promise，可能已经就绪，也可能以后就绪
-// add(fetchX(), fetchY()).then(function(sum) {
-//     console.log("sum: ", sum);  // sum:  204
-// });
-
-const a = {};
-const b = {key: 'b'};
-const c = {key: 'c'};
-a[b] = 123;
-a[c] = 456;
-console.log(a[b]);
-
-
-
-
-Function.prototype.bind = function() {
-    // 保存原函数
-    let self = this;
-    // 需要绑定的 this 上下文
-    let context = [].shift.call(arguments);
-    // 剩余的参数转成数组
-    let args = [].slice.call(arguments);
-    return function() {
-        // 执行新的函数的时候，会把之前传入的 context 当作新参数体内的 this
-        // 并且组合两次分别传入的参数，作为新函数的参数
-        return self.apply(context, [].concat.call(args, [].slice.call(arguments)))
-    }
-};
-
-const person = { name: "Lydia" };
-
-function sayHi(age) {
-    console.log(`${this.name} is ${age}`);
+function fetchX() {
+    return new Promise(function (resolve, reject) {
+        resolve(200)
+    })
+}
+function fetchY() {
+    return new Promise(function (resolve, reject) {
+        resolve(4)
+    })
 }
 
-// sayHi.call(person, 21);
-sayHi.bind(person, 21);
+// fetchX() 和 fetchY() 返回相应值的 promise，可能已经就绪，也可能以后就绪
+add(fetchX(), fetchY()).then(function(sum) {
+    console.log("sum: ", sum);  // sum:  204
+});
