171815daf2983c44901c3a39be302d73d5f05b6b
PayneW <2416451355@qq.com>
2019-07-16 01:43:07 +0800

    mac edit: 20190715

---
+++ 《JavaScript编程思想从ES5到ES9》/Reflect-对象.js
@@ -0,0 +1,53 @@
+
+// 13-1_Reflect-object.js
+let candy_amount = {
+    durian: 30,
+    strawberry: 55,
+    cranberry: 10,
+    blueberry: 13,
+    cherry: 60,
+    orange: 18,
+    lemon: 10
+};
+
+Object.defineProperty(candy_amount, 'apple', {
+    value: 17,
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+candy_amount.watermelon = 33;
+
+// 初始化数据为内含字符串 'mixed' 的 Symbol 对象实例
+let symbol01 = Symbol('mixed');
+
+candy_amount[symbol01] = 5;
+
+
+// 内置函数 Reflect.ownKeys() 可使 candy_amount 对象中的各个属性组成一个数组
+// [
+//   'durian',     'strawberry',
+//   'cranberry',  'blueberry',
+//   'cherry',     'orange',
+//   'lemon',      'apple',
+//   'watermelon', Symbol(mixed)
+// ]
+console.log(Reflect.ownKeys(candy_amount));
+console.log('');
+
+console.log(candy_amount.orange);
+console.log(candy_amount.watermelon);
+console.log('');
+
+// .mixed/['mixed'] 都返回一个 undefined 表示在 candy_amount 中不存在属性 mixed
+console.log(candy_amount.mixed);
+console.log(candy_amount['mixed']);
+console.log('');
+
+// 返回对象中标识符为 Symbol('mixed') 的属性
+console.log(candy_amount[symbol01]);
+
+
+
+
