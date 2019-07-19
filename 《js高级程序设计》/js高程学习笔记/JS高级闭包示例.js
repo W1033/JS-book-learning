0a05c130454201a039c91d43a09f5618877df54f
PayneW <2416451355@qq.com>
2019-07-19 22:42:55 +0800

    Mac edit:20190719

--- 《js高级程序设计》/js高程学习笔记/JS高级闭包示例.js
+++ 《js高级程序设计》/js高程学习笔记/JS高级闭包示例.js
@@ -12,13 +12,14 @@ function closure() {
 
 var returnArr = closure();
 for (var i = 0; i < returnArr.length; i++) {
-    console.log(returnArr[i]());//调用时不要忘记后面的()小括号，这样returnArr[i]只是调用了函数的指针。
+    // 调用时不要忘记后面的()小括号，这样returnArr[i]只是调用了函数的指针。
+    console.log(returnArr[i]());
 }
 
 console.log("-------------------------------------");
 
 
-//改进闭包问题，输出正常值
+// 改进闭包问题，输出正常值
 function closureConversionNormal() {
     var arr = [];
     for (var i = 0; i < 10; i++) {
