d3b50ac690485c3810a79df5505f35a63f444ce3
PayneW <2416451355@qq.com>
2019-08-04 23:24:12 +0800

    mac edit: 20190804 01

---
+++ <石川讲师>-Video/2-11-Generate/runner.js
@@ -0,0 +1,32 @@
+function runner(_gen) {
+    return new Promise((resolve, reject) => {
+        let gen = _gen();
+
+        _next();
+
+        function _next(_last_res) {
+            let res = gen.next(_last_res);
+
+            if (!res.done) {
+                let obj = res.value;
+
+                if (obj.then) {
+                    obj.then((res) => {
+                        _next(res);
+                    }, (err) => {
+                        reject(err);
+                    })
+                } else if (typeof obj === 'function') {
+                    if (obj.constructor.toString().startsWith(
+                        'function GeneratorFunction()')) {
+                        runner(obj).then(res => _next(res), reject);
+                    } else {
+                        _next(obj());
+                    }
+                } else {
+                    resolve(res, value);
+                }
+            }
+        }
+    })
+}
