c0b7c04a6ec30f3dd5632e2662cf1276dc2769c9
2416451355@qq.com <314314@wang>
2019-08-10 23:30:39 +0800

    win 20190810 02

---
+++ 《深入理解ES6》/11th-chapter--Promise/Promise.js
@@ -0,0 +1,35 @@
+(function(scope) {
+    const PENDING = 'pending';
+    const FULFILLED = 'fulfilled';
+    const REJECTED = 'rejected';
+
+    class Promise{
+        constructor(resolver) {}
+
+        executeRsolve(resolver) {}
+
+        getThen(value) {}
+
+        executeCallback() {}
+
+        executeAsyncCallback() {}
+
+        then() {}
+
+        catch() {}
+
+        // - 非 ES6 标准
+        done() {}
+
+    }
+
+    class CallbackItem {
+
+    }
+
+    // - Promise 类上添加的静态方法(类方法)
+    Promise.resolve = function() {};
+
+
+
+})(this);
