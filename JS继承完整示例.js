64ff2432658578821f940b5bdaab8a8eb797f619
2416451355@qq.com <314314@wang>
2019-08-17 11:38:33 +0800

    win 20190817 04

--- JS继承完整示例.js
+++ JS继承完整示例.js
@@ -5,7 +5,8 @@ const A = function (name) {
 };
 
 const B = function () {
-    // B 构造器借用 A 构造器的 name 属性
+    // - B 构造器借用 A 构造器的 name 属性
+    // - 继承父构造函数的属性
     A.apply(this, arguments);
 };
 
@@ -126,10 +127,10 @@ function Square(length) {
     Rectangle.call(this, length, length);
 }
 
-// - Note: 《JS高程》--6.3.4-原型式继承-P170：ES5 通过新增 Object.create(): 方法规规范
-//   化了原型式继承。这个方法接收 2 个参数: (1).用作新对象原型的对象 和 (2).一个(可选的)
-//   为新对象定义额外属性的对象。在传入一个参数的情况下 Object.create() 与 Object() 方法
-//   的行为相同。
+// - Note: 《JS高程》--6.3.4-原型式继承-P170：ES5 通过新增 Object.create(): 方法规范
+//   化了原型式继承。这个方法接收 2 个参数: (1)用作新对象原型的对象 和 (2)一个(可选的)
+//   为新对象定义额外属性的对象。在传入一个参数的情况下 Object.create() 与 Object()
+//   方法的行为相同。
 Square.prototype = Object.create(Rectangle.prototype, {
     constructor: {
         // 这种写法等于: Square.prototype.constructor = Square
