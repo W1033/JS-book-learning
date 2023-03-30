ae426d39b93bf4339dd7cb63efee112e2910c2bb
W1033 <forownwang@gmail.com>
2023-03-30 17:14:23 +0800

    2023.03.30

--- 《深入理解ES6》/chapter06_Symbol和Symbol属性/chapter06-run-demo-code.js
+++ 《深入理解ES6》/chapter06_Symbol和Symbol属性/chapter06-run-demo-code.js
@@ -4,4 +4,23 @@ person[firstName] = "Nicholas";
 
 // - 编辑器内输出为: {[Symbol()]: 'Nicholas'}
 // - 浏览器内输出为: {Symbol(): "Nicholas"}
-console.log(person);
+// console.log(person);
+
+
+
+let MyClass = (function() {
+    // module scoped symbol
+    const key = Symbol('key');
+    function MyClass(privateData) {
+        this[key] = privateData;
+    }
+    MyClass.prototype = {
+        constructor: MyClass,
+        doStuff: function() {
+            // ... this[key] ...
+        }
+    };
+    return MyClass;
+})();
+let c = new MyClass('Hello');
+console.log(c['key'] === "key");
