d13668a0b5790eefc15fe5232715fbecf555bf54
W1033 <forownwang@gmail.com>
2023-04-05 22:19:43 +0800

    2023.04.05

--- JavaScript知识集合/JavaScript 倒计时/count-down-assets/countdown.js
+++ JavaScript知识集合/JavaScript 倒计时/count-down-assets/countdown.js
@@ -1,59 +1,59 @@
 const displayMinutes = document.querySelector('.timer-minutes');
-        const displaySeconds = document.querySelector('.timer-seconds');
+const displaySeconds = document.querySelector('.timer-seconds');
 
-        let intervalId;
-        let startTime;
-        let timerDuration;
+let intervalId;
+let startTime;
+let timerDuration;
 
-        function startTimer() {
-            if (intervalId) return;
+function startTimer() {
+    if (intervalId) return;
 
-            startTime = Date.now();
-            intervalId = setInterval(() => {
-                const elapsedMilliseconds = Date.now() - startTime;
-                const remainingMilliseconds = timerDuration - elapsedMilliseconds;
+    startTime = Date.now();
+    intervalId = setInterval(() => {
+        const elapsedMilliseconds = Date.now() - startTime;
+        const remainingMilliseconds = timerDuration - elapsedMilliseconds;
 
-                if (remainingMilliseconds <= 0) {
-                    stopTimer();
-                    return;
-                }
-
-                const remainingSeconds = Math.floor(remainingMilliseconds / 1000) % 60;
-                const remainingMinutes = Math.floor(remainingMilliseconds / 1000 / 60);
-                displayMinutes.textContent = remainingMinutes.toString().padStart(2, '0');
-                displaySeconds.textContent = remainingSeconds.toString().padStart(2, '0');
-            }, 10);
-        }
-
-        function stopTimer() {
-            clearInterval(intervalId);
-            intervalId = null;
-        }
-
-        function resetTimer() {
+        if (remainingMilliseconds <= 0) {
             stopTimer();
-            displayMinutes.textContent = '00';
-            displaySeconds.textContent = '00';
-            timerDuration = 0;
+            return;
         }
 
-        function setTimerDuration(minutes, seconds) {
-            timerDuration = (minutes * 60 + seconds) * 1000;
-            resetTimer();
-        }
+        const remainingSeconds = Math.floor(remainingMilliseconds / 1000) % 60;
+        const remainingMinutes = Math.floor(remainingMilliseconds / 1000 / 60);
+        displayMinutes.textContent = remainingMinutes.toString().padStart(2, '0');
+        displaySeconds.textContent = remainingSeconds.toString().padStart(2, '0');
+    }, 10);
+}
 
-        document.querySelector('.start-btn').addEventListener('click', startTimer);
-        document.querySelector('.pause-btn').addEventListener('click', stopTimer);
-        document.querySelector('.reset-btn').addEventListener('click', resetTimer);
+function stopTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
 
-        document.querySelector('#minutes-input').addEventListener('change', (event) => {
-            const minutes = parseInt(event.target.value);
-            const seconds = parseInt(document.querySelector('#seconds-input').value);
-            setTimerDuration(minutes, seconds);
-        });
+function resetTimer() {
+    stopTimer();
+    displayMinutes.textContent = '00';
+    displaySeconds.textContent = '00';
+    timerDuration = 0;
+}
 
-        document.querySelector('#seconds-input').addEventListener('change', (event) => {
-            const seconds = parseInt(event.target.value);
-            const minutes = parseInt(document.querySelector('#minutes-input').value);
-            setTimerDuration(minutes, seconds);
-        });
+function setTimerDuration(minutes, seconds) {
+    timerDuration = (minutes * 60 + seconds) * 1000;
+    resetTimer();
+}
+
+document.querySelector('.start-btn').addEventListener('click', startTimer);
+document.querySelector('.pause-btn').addEventListener('click', stopTimer);
+document.querySelector('.reset-btn').addEventListener('click', resetTimer);
+
+document.querySelector('#minutes-input').addEventListener('change', (event) => {
+    const minutes = parseInt(event.target.value);
+    const seconds = parseInt(document.querySelector('#seconds-input').value);
+    setTimerDuration(minutes, seconds);
+});
+
+document.querySelector('#seconds-input').addEventListener('change', (event) => {
+    const seconds = parseInt(event.target.value);
+    const minutes = parseInt(document.querySelector('#minutes-input').value);
+    setTimerDuration(minutes, seconds);
+});
